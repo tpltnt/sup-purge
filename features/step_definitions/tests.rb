0ba01065a4745203c1bcffa789a5d1d08ab7f6d7
tpltnt <tpltnt.github@dropcut.net>
2012-02-20 23:22:42 +0100

    test: puts -> system

--- features/step_definitions/tests.rb
+++ features/step_definitions/tests.rb
@@ -3,9 +3,9 @@ Given /^I have a dumpfile$/ do
 end
 
 When /^i call the script with the dumpfile as an argument$/ do
-  puts `./sup-purge supdump`
+  system './sup-purge features/step_definitions/supdump'
 end
 
 Then /^i should be able to open the dumpfile$/ do
-  pending # express the regexp above with the code you wish you had
+  system './sup-purge features/step_definitions/supdump'
 end
