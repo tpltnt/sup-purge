2afc0e06f8cc64c2cc54ffadab5742e7e3b45a1b
tpltnt <tpltnt.github@dropcut.net>
2012-02-21 20:40:36 +0100

    dumpfilename cleanup

--- features/step_definitions/tests.rb
+++ features/step_definitions/tests.rb
@@ -2,30 +2,28 @@
 require 'test/unit/assertions'
 World(Test::Unit::Assertions)
 
+
+dumpfilename = 'features/step_definitions/supdump'
+
 Given /^I have a dumpfile$/ do
-  @dumpfilename = 'features/step_definitions/supdump'
-  assert( File.exists?(@dumpfilename) )
+  assert( File.exists?(dumpfilename) )
 end
 
 When /^i call the script with the dumpfile as an argument$/ do
-  @dumpfilename = 'features/step_definitions/supdump'
-  assert( system "./sup-purge #{@dumpfilename}" )
+  assert( system "./sup-purge #{dumpfilename}" )
 end
 
 Then /^i should be able to open the dumpfile$/ do
-  @dumpfilename = 'features/step_definitions/supdump'
-  assert( system "./sup-purge #{@dumpfilename}" )
+  assert( system "./sup-purge #{dumpfilename}" )
 end
 
 When /^I open the file$/ do
-  @dumpfilename = 'features/step_definitions/supdump'
-  @dumpfile = File.open(@dumpfilename)
+  @dumpfile = File.open(dumpfilename)
 end
 
 Then /^each line should follow a pattern$/ do
   @allok = true
-  @dumpfilename = 'features/step_definitions/supdump'
-  @dumpfile = File.open(@dumpfilename)
+  @dumpfile = File.open(dumpfilename)
   # test each line for the pattern
   @dumpfile.each do |line|
     # pattern: random_string@random_string+" "+"("tag1" "tag2" "tagn")"
